4f4aa2e9b6e6cc8981a920ecf1fbf7abbf8f6c65
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2019-12-03 17:14:41 +0200

    MatchWithOneChar

--- ClassRange/ClassRange/Range.cs
+++ ClassRange/ClassRange/Range.cs
@@ -1,10 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ClassRange
 {
-    class Range
+    public class Range
     {
+        private readonly char start;
+        private readonly char end;
+
+        public Range(char startText, char endText)
+        {
+            this.start = startText;
+            this.end = endText;
+        }
+
+        public bool Match(string text)
+        {
+            return IsInRange(start, end, text);
+        }
+
+        private bool IsInRange(char start, char end, string text)
+        {
+            return Convert.ToChar(text) >= start && Convert.ToChar(text) <= end;
+        }
     }
 }
---
+++ ClassRange/ClassRange/RangeTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace ClassRange
+{
+    public class RangeTests
+    {
+        [Fact]
+        public void ChecIfTextMatch()
+        {
+            Range match = new Range('a', 'z');
+
+            Assert.True(match.Match("b"));
+        }
+    }
+}
