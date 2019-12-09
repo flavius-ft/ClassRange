5bfa1ca99a5a426a54ebc55cf80b84433cb96629
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2019-12-09 16:47:36 +0200

    Class Character

---
+++ ClassRange/ClassRange/Character.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassRange
+{
+    class Character
+    {
+        readonly char pattern;
+
+        public Character(char pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            return text[0] == pattern;
+        }
+    }
+}
