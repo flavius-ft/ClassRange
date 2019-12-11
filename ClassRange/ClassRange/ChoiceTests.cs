6c81e4245fb0428ee75a9ddae268c2fe5bd9a5a1
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2019-12-11 16:07:35 +0200

    Choice tests

--- ClassRange/ClassRange/ChoiceTests.cs
+++ ClassRange/ClassRange/ChoiceTests.cs
@@ -5,13 +5,55 @@ namespace ClassRange
     public class ChoiceTests
     {
         [Fact]
-        public void CheckDigits()
+        public void CheckDigitsWithZeroFirst()
         {
             var digit = new Choice(
                 new Character('0'),
                 new Range('1', '9'));
 
             Assert.True(digit.Match("0159"));
+        }
+
+        [Fact]
+        public void CheckDigitsWithOutZero()
+        {
+            var digit = new Choice(
+                new Character('0'),
+                new Range('1', '9'));
+
+            Assert.True(digit.Match("159"));
+        }
+
+        [Fact]
+        public void UseMatchOnAStringWithNumberAndALetter()
+        {
+            var digit = new Choice(
+                new Character('0'),
+                new Range('1', '9'));
+
+            Assert.False(digit.Match("a9"));
+        }
+
+        [Fact]
+        public void UseMatchToAnEmptyString()
+        {
+            var digit = new Choice(
+                new Character('0'),
+                new Range('1', '9'));
+
+            Assert.False(digit.Match(""));
+        }
+
+        [Fact]
+        public void UseMatchForANullString()
+        {
+            var digit = new Choice(
+                new Character('0'),
+                new Range('1', '9'));
+
+            const string nullString = null;
+
+            Assert.False(digit.Match(nullString));
         }
     }
 }
