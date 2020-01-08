69f74eb6e68371152aa42b684f0ab6e393bf9beb
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-08 14:35:12 +0200

    mroe tests on Sequence

--- ClassRange/ClassRange/SequenceTests.cs
+++ ClassRange/ClassRange/SequenceTests.cs
@@ -81,5 +81,82 @@ namespace ClassRange
 
             Assert.Equal("ef", hexSeq.Match("uabcdef").RemainingText());
         }
+
+        [Fact]
+        public void UseHexDigitsOnSequenceMethodWhenCharUIsMissingReturnFalse()
+        {
+            var hex = new Choice(
+               new Range('0', '9'),
+               new Range('a', 'f'),
+               new Range('A', 'F'));
+
+            var hexSeq = new Sequence(
+                new Character('u'),
+                new Sequence(
+                    hex,
+                    hex,
+                    hex,
+                    hex));
+
+            Assert.False(hexSeq.Match("abc").Success());
+        }
+
+        [Fact]
+        public void UseHexDigitsOnSequenceMethodWhenIsFalseReturnningFullText()
+        {
+            var hex = new Choice(
+               new Range('0', '9'),
+               new Range('a', 'f'),
+               new Range('A', 'F'));
+
+            var hexSeq = new Sequence(
+                new Character('u'),
+                new Sequence(
+                    hex,
+                    hex,
+                    hex,
+                    hex));
+
+            Assert.Equal("abc", hexSeq.Match("abc").RemainingText());
+        }
+
+        [Fact]
+        public void UseHexDigitsOnSequenceMethodRWithEmptyStringReturnEmptyString()
+        {
+            var hex = new Choice(
+               new Range('0', '9'),
+               new Range('a', 'f'),
+               new Range('A', 'F'));
+
+            var hexSeq = new Sequence(
+                new Character('u'),
+                new Sequence(
+                    hex,
+                    hex,
+                    hex,
+                    hex));
+
+            Assert.Equal("", hexSeq.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void UseHexDigitsOnSequenceMethodWithNullStringReturnNullString()
+        {
+            const string isNull = null;
+            var hex = new Choice(
+               new Range('0', '9'),
+               new Range('a', 'f'),
+               new Range('A', 'F'));
+
+            var hexSeq = new Sequence(
+                new Character('u'),
+                new Sequence(
+                    hex,
+                    hex,
+                    hex,
+                    hex));
+
+            Assert.Equal(null, hexSeq.Match(isNull).RemainingText());
+        }
     }
 }
