5c8276e562ad3eb508331dc46965a808991285e0
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-11 13:06:36 +0200

    ClassText WithTests

---
+++ ClassRange/ClassRange/Text.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassRange
+{
+    class Text : IPattern
+    {
+        readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            return text?.Substring(0, prefix.Length).Equals(prefix) == true ? new Match(true, text.Substring(prefix.Length))
+                    : new Match(false, text);
+        }
+    }
+}
---
+++ ClassRange/ClassRange/TextTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace ClassRange
+{
+    public class TextTests
+    {
+        [Fact]
+        public void AplyTextMethodOnAStringreturnTrue()
+        {
+            var prefix = new Text("pre");
+
+            Assert.True(prefix.Match("preposition").Success());
+        }
+
+        [Fact]
+        public void AplyTextMethodOnAStringreturnFalse()
+        {
+            var prefix = new Text("pre");
+
+            Assert.False(prefix.Match("ppreposition").Success());
+        }
+
+        [Fact]
+        public void ReturnRemainingTextAfterMethodTextIsAplayed()
+        {
+            var pref = new Text("pre");
+
+            Assert.Equal("position", pref.Match("preposition").RemainingText());
+        }
+
+        [Fact]
+        public void ReturnRemainingTextAfterMethodTextIsAplayedOnAnEmtyString()
+        {
+            var pref = new Text("");
+
+            Assert.Equal("preposition", pref.Match("preposition").RemainingText());
+        }
+    }
+}
