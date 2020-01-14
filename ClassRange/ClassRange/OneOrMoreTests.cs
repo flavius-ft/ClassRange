5f96cbda891a24a85d4e440f86b773a4eb240a02
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-14 11:41:06 +0200

    OneOreMore Test with Range

---
+++ ClassRange/ClassRange/OneOrMoreTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace ClassRange
+{
+    public class OneOrMoreTests
+    {
+        [Fact]
+        public void UseOneOrMoreClassOnAGivenStringReturnRemainingText()
+        {
+            var oneOrMore = new OneOrMore(new Range('0', '9'));
+
+            Assert.Equal("ab", oneOrMore.Match("123ab").RemainingText());
+        }
+    }
+}
