0102abc295ae55cf53199cfaf126e035b2c3b027
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-14 11:49:03 +0200

    Tests withSequence

--- ClassRange/ClassRange/OneOrMoreTests.cs
+++ ClassRange/ClassRange/OneOrMoreTests.cs
@@ -5,11 +5,30 @@ namespace ClassRange
     public class OneOrMoreTests
     {
         [Fact]
-        public void UseOneOrMoreClassOnAGivenStringReturnRemainingText()
+        public void UseOneOrMoreClassWithRangeOnAGivenStringReturnRemainingText()
         {
             var oneOrMore = new OneOrMore(new Range('0', '9'));
 
             Assert.Equal("ab", oneOrMore.Match("123ab").RemainingText());
+        }
+
+        [Fact]
+        public void UseOneOrMoreClassWithCharacterOnAGivenStringReturnRemainingText()
+        {
+            var oneOrMore = new OneOrMore(new Character('a'));
+
+            Assert.Equal("b", oneOrMore.Match("aaab").RemainingText());
+        }
+
+        [Fact]
+        public void UseOneOrMoreClassWithSequenceOnAGivenStringReturnRemainingText()
+        {
+            var oneOrMore = new OneOrMore(new Sequence(
+                new Character('u'),
+                new Range('0', '9'),
+                new Range('0', '9')));
+
+            Assert.Equal("b", oneOrMore.Match("u12b").RemainingText());
         }
     }
 }
