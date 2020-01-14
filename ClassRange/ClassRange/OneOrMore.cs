db0d2c367481fa0a27d1d2767970c27245d8c492
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-14 11:14:09 +0200

    Class OneOrMore

---
+++ ClassRange/ClassRange/OneOrMore.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassRange
+{
+    class OneOrMore : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Many(pattern);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
