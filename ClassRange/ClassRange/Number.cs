5b6ec90059e03fb8481da692dcc8ecb948d8f1b9
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-16 17:39:46 +0200

    CLass Number

---
+++ ClassRange/ClassRange/Number.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassRange
+{
+    class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+            pattern = new Range('0', '9');
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
