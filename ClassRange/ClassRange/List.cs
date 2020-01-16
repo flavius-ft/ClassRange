54496836b1ac5d151ecdfdc42da12f03bb9ef449
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-16 15:29:06 +0200

    ClassList

---
+++ ClassRange/ClassRange/List.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassRange
+{
+    class List : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = new Sequence(element, new Many(new Sequence(separator, element)));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ ClassRange/ClassRange/ListTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace ClassRange
+{
+    public class ListTests
+    {
+        [Fact]
+        public void UseListMethodReturnRemainingText()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+
+            Assert.Equal(",", a.Match("1,2,3,").RemainingText());
+        }
+
+        [Fact]
+        public void UseListMethodWithStringReturnRemainingText()
+        {
+            var a = new List(new Range('0', '9'), new Character(','));
+
+            Assert.Equal("a", a.Match("1a").RemainingText());
+        }
+
+        [Fact]
+        public void UseListMethodWithComplexStringReturnRemainingText()
+        {
+            var digits = new OneOrMore(new Range('0', '9'));
+            var whitespace = new Many(new Any(" \r\n\t"));
+            var separator = new Sequence(whitespace, new Character(';'), whitespace);
+            var list = new List(digits, separator);
+
+            Assert.Equal("", list.Match("1; 22;\n 333 \t; 22").RemainingText());
+        }
+    }
+}
