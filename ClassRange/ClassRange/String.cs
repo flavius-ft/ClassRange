130fe24561b8916fd1646f0ddf8353cf596c9890
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-21 15:13:46 +0200

    String Class

---
+++ ClassRange/ClassRange/String.cs
@@ -0,0 +1,46 @@
+﻿namespace ClassRange
+{
+    class String : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public String()
+        {
+            var quotationMark = new Character('"');
+            var controlChars = new Any("\\\"/bfnrt");
+            var charsRange = new Choice(
+                new Range(' ', '!'),
+                new Range('#', '['),
+                new Range(']', '⌂'));
+            var chars = new OneOrMore(charsRange);
+            var hex = new Choice(
+                new Range('0', '9'),
+                new Range('a', 'f'),
+                new Range('A', 'F'));
+            var hexaDigits = new Sequence(
+                new Character('u'),
+                hex,
+                hex,
+                hex,
+                hex);
+            var backSlash = new Sequence(
+                new Character('\\'),
+                new Optional(controlChars),
+                new Optional(hexaDigits));
+            var stringChars = new Many(
+                new Choice(
+                    chars,
+                    backSlash));
+
+            pattern = new Sequence(
+                quotationMark,
+                stringChars,
+                quotationMark);
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ ClassRange/ClassRange/StringTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace ClassRange
+{
+    public class StringTests
+    {
+        [Fact]
+        public void TestStringClassWithSimpleString()
+        {
+            var text = new String();
+
+            Assert.Equal("", text.Match("\"abc\"").RemainingText());
+        }
+
+        [Fact]
+        public void TestStringClassWithcOMPLEXString()
+        {
+            var text = new String();
+
+            Assert.Equal("", text.Match("\"a\\u0015b\\u0010c\"").RemainingText());
+        }
+    }
+}
