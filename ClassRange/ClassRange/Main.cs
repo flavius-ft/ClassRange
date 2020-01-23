86f64b2dfbcf804134430403969851c4eb303a6c
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2020-01-23 16:51:57 +0200

    Class Main

---
+++ ClassRange/ClassRange/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassRange
+{
+    public class Program
+    {
+        internal static void Main()
+        {
+            string text = System.IO.File.ReadAllText(@"E:\JsonText.txt");
+
+            Console.WriteLine(new Value().Match(text).Success() ? "Valid" : "Invalid");
+
+            Console.Read();
+        }
+    }
+}
